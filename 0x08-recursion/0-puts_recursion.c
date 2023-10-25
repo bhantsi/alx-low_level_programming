566e6f0d466b6b55f2e7672219c7e2a24ce34bb2
Hamza <bhantsi@gmail.com>
2023-10-25 12:31:02 +0100

    Add task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _put_recursion - Print a string follwed by a newline
+ * @s: the string to print
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s);
+{
+if
+(*s == '\0');
+
+{
+_putchar('\n')
+return;
+}
+
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
