02bde3eb09578798baeb917d04d64253ef8168ce
Hamza <bhantsi@gmail.com>
2023-10-28 01:19:18 +0100

    Add advance task solution

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+int cents;
+int coins[5] = {25, 10, 5, 2, 1};
+int num_coins = 0;
+int i;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+
+for (i = 0; i < 5; i++)
+{
+while (cents >= coins[i])
+{
+cents -= coins[i];
+num_coins++;
+}
+}
+
+printf("%d\n", num_coins);
+return (0);
+}
+
