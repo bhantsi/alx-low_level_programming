200de772e0739fe54430198d131a29a76b723208
Hamza <bhantsi@gmail.com>
2023-11-06 19:14:29 +0100

    Add task 0 && 1 solutions

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's characteristics.
+ * @name: The dog's name (a string).
+ * @age: The dog's age (a floating-point number).
+ * @owner: The dog's owner (a string).
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+/* Function prototype*/
+void int_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
+
