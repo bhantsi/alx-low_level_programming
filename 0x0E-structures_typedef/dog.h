3a6484abfce1352d050271dcd51232c59986de2a
Hamza <bhantsi@gmail.com>
2023-11-08 00:35:11 +0100

    Add task 5 solutions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ void int_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
 
